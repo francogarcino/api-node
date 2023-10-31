80de3bc67f5412248e825cbb5b06e78cc3041c64
francogarcino <89816641+francogarcino@users.noreply.github.com>
2023-10-30 23:27:53 -0300

    add classes to items and damage

--- epers-online/src/model/item/item.schema.ts
+++ epers-online/src/model/item/item.schema.ts
@@ -5,12 +5,26 @@ export type ItemDoc = HydratedDocument<Item>;
 
 @Schema({collection: 'items', versionKey: false})
 export class Item {
-    constructor(nombre: string) {
+    constructor(nombre: string, clase: RelatedClass, daño: number) {
         this.name = nombre
+        this.class = clase
+        if (daño < 1 && (clase != RelatedClass.HEALER || clase != RelatedClass.WIZARD)) {
+            throw new Error("Damage can not be zero or negative for an melee or marksman item")
+        }
+        this.damage = daño
     }
 
     _id: String
-    @Prop() name: string;
+    @Prop() name: string
+    @Prop() class: RelatedClass
+    @Prop() damage = daño
+    // proba si funciona sin prop por CoC
+    owner?: string;
+
 }
 
 export const ItemSchema = SchemaFactory.createForClass(Item);
+
+enum RelatedClass {
+    MELEE, WIZARD, HEALER, MARKSMAN
+}
