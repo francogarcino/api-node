3e687dcb3b12bd1db93c2b4a2ed57bc05d5d911f
Franco Garcino Ruiz <francogarcino@gmail.com>
2023-11-03 00:50:56 -0300

    test 2

--- epers-online/src/app.controller.ts
+++ epers-online/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Logger } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  logger = new Logger("AppController")
   @Get()
   getHello(): string {
+    this.logger.log("hello")
     return this.appService.getHello();
   }
 }
